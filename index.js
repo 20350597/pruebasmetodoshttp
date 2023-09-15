f731d42b9b60682ccabe9b79142b995202b9c16e
Carlos Juarez Peña <isc20350597@gmail.com>
2023-09-15 09:47:26 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express') //importando el módulo express
+
+const app = express()
+//http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send("Hola con GET")
+})
+/* Los navegadores sólo usan el método GET, por lo tanto los demás no se verán */
+app.post('/', (req, res) => {
+    res.send("Hola con POST")
+})
+
+app.put('/', (req, res) => {
+    res.send("Hola con PUT")
+})
+
+app.patch('/', (req, res) => {
+    res.send("Hola con PATCH")
+})
+
+app.delete('/', (req, res) => {
+    res.send("Hola con DELETE")
+})
+
+
+
+
+
+app.listen(3000, () => {console.log('listening on port 3000')})
